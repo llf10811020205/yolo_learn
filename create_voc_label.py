8c12c11910599cc17f3fac5b981756dcb52cb5cd
llf <llf_tbd@aliyun.com>
2018-05-15 10:29:48 +0800

    开源数据集coco kitti voc相关脚本处理

---
+++ create_voc_label.py
@@ -0,0 +1,66 @@
+import xml.etree.ElementTree as ET
+import pickle
+import os
+from os import listdir, getcwd
+from os.path import join
+
+sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+
+classes = ["person", "bicycle","car", "motorbike",  "bus", "truck"]
+#classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+
+def convert(size, box):
+    dw = 1./(size[0])
+    dh = 1./(size[1])
+    x = (box[0] + box[1])/2.0 - 1
+    y = (box[2] + box[3])/2.0 - 1
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+    return (x,y,w,h)
+
+def convert_annotation(year, image_id):
+    in_file = open('VOC%s/Annotations/%s.xml'%(year, image_id))
+    out_file = open('VOC%s/labels/%s.txt'%(year, image_id), 'w')
+    tree=ET.parse(in_file)
+    root = tree.getroot()
+    size = root.find('size')
+    w = int(size.find('width').text)
+    h = int(size.find('height').text)
+    valid_class_count = 0
+
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult)==1:
+            continue
+        valid_class_count = valid_class_count + 1
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
+        bb = convert((w,h), b)
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+    if valid_class_count == 0:
+        os.remove('VOC%s/labels/%s.txt'%(year, image_id))
+        return False
+    return True
+
+wd = getcwd()
+
+for year, image_set in sets:
+    if not os.path.exists('VOC%s/labels/'%(year)):
+        os.makedirs('VOC%s/labels/'%(year))
+    image_ids = open('VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
+    list_file = open('%s_%s.txt'%(year, image_set), 'w')
+    for image_id in image_ids:
+        suc = convert_annotation(year, image_id)
+        if suc == True:
+            list_file.write('%s/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
+    list_file.close()
+
+os.system("cat 2007_train.txt 2007_val.txt 2012_train.txt 2012_val.txt > train_val.txt")
+os.system("cat 2007_train.txt 2007_val.txt 2007_test.txt 2012_train.txt 2012_val.txt > trainall.txt")
+
